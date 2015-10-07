d6261f683ca43d5c82778ff7e423f5dd0d25984c
saquiba <saquiba@sbcglobal.net>
2015-10-06 20:42:56 -0700

    Test.php
    File should cause a build fail since one plus one does not equal five.

---
+++ Test.php
@@ -0,0 +1,12 @@
+<?php 
+  
+class Test extends PHPUnit_Framework_TestCase 
+{ 
+	public function testOnePlusOne() { 
+	
+	  #originally: $this->assertEquals(1+1,5); 
+		$this->assertEquals(1+1,5); 
+ 	} 
+} 
+
+?> 
